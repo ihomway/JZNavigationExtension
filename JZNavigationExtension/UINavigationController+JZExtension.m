5100eb7c2caece250a15e252708f16ad0b66ee7e
ihomway <ihomway@live.com>
2018-05-10 11:08:13 +0800

    remove the `NSAssert`
    ```
    // NSAssert(!self.delegate, @"Set delegate should be invoked when viewDidLoad");
    self.delegate = self.delegate;
    ```
    
    remove the `NSAssert`, otherwise the app will crash when using system image picker view controller

--- JZNavigationExtension/UINavigationController+JZExtension.m
+++ JZNavigationExtension/UINavigationController+JZExtension.m
@@ -55,8 +55,8 @@ __attribute__((constructor)) static void JZ_Inject(void) {
 }
 
 - (void)jz_viewDidLoad {
-    NSAssert(!self.delegate, @"Set delegate should be invoked when viewDidLoad");
-    self.delegate = nil;
+    // NSAssert(!self.delegate, @"Set delegate should be invoked when viewDidLoad");
+    self.delegate = self.delegate;
     [self.interactivePopGestureRecognizer setValue:@NO forKey:@"canPanVertically"];
     self.interactivePopGestureRecognizer.delegate = self.jz_navigationDelegate;
     [self jz_viewDidLoad];
